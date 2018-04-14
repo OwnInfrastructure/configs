ac3dcada76db19945e981ba78dd175edbee3e2a8
Viacheslav Kovalevskyi <vkovalevskyi@google.com>
2018-04-14 07:53:21 -0700

    list_zones command added

--- gcloud.sh
+++ gcloud.sh
@@ -3,6 +3,11 @@ function list_regions() {
 	echo $REGIONS | cut -d " " -f 1 | tail -n +2
 }
 
+function list_zones() {
+	ZONES=$(gcloud compute zones list)
+	echo $ZONES | cut -d " " -f 1 | tail -n +2
+}
+
 alias current_project="gcloud config get-value project"
 alias list_projects="gcloud projects list"
 
