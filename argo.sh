36878bbb73954939d34f65d0b7f02edbfc25fb09
Viacheslav Kovalevskyi <viacheslav@kovalevskyi.com>
2018-07-16 14:10:54 -0700

    argo_enable_ui added.

--- argo.sh
+++ argo.sh
@@ -5,3 +5,8 @@ function argo_watch() {
 function argo_logs() {
   argo logs -f -w $1
 }
+
+function argo_enable_ui () {
+	echo "visit: http://127.0.0.1:8001/"
+	kubectl port-forward $(kubectl get pods -n kube-system -l app=argo-ui -o jsonpath='{.items[0].metadata.name}') -n kube-system 8001:8001
+}
