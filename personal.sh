7babddadf2907b57b89421991673d20713698372
Viacheslav Kovalevskyi <vkovalevskyi@google.com>
2020-06-13 10:01:00 -0700

    personal, non generic, scripts are added

---
+++ personal.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+source gcloud.sh
+
+function upload_public_tmp() {
+	FILE_TO_UPLOAD="${1}"
+	UPLOAD_REMOTE="gs://b0noi-tmp/${FILE_TO_UPLOAD}"
+	LINK="https://storage.googleapis.com/b0noi-tmp/tlm/${FILE_TO_UPLOAD}"
+	gsutil -o GSUtil:parallel_composite_upload_threshold=500M cp "${FILE_TO_UPLOAD}" "${UPLOAD_REMOTE}"
+	make_public "${UPLOAD_REMOTE}"
+	echo "link: ${LINK}"
+}
